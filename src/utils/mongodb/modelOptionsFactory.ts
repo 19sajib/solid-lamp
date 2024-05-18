7997516c67ff6e085ee2d74d597f987c7d405bef
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-18 16:35:52 +0600

    typegoose modelOptions

---
+++ src/utils/mongodb/modelOptionsFactory.ts
@@ -0,0 +1,36 @@
+import { IModelOptions } from "@typegoose/typegoose/lib/types";
+
+export const modelOptionsFactory = (
+    collection: string,
+    timestamps: boolean,
+    deleteTimestamps= false
+) : IModelOptions => {
+    return {
+        schemaOptions: {
+            collection,
+            toJSON: {
+                virtuals: true,
+                transform: (_, ret) => {
+                    ret.id = ret._id;
+					delete ret._id;
+					delete ret.__v;
+					deleteTimestamps ? delete ret?.createdAt : null;
+					deleteTimestamps ? delete ret?.updatedAt : null;
+					return ret;
+                }
+            },
+            toObject: {
+				virtuals: true,
+				transform: (_, ret) => {
+					ret.id = ret._id;
+					delete ret._id;
+					delete ret.__v;
+					deleteTimestamps ? delete ret?.createdAt : null;
+					deleteTimestamps ? delete ret?.updatedAt : null;
+					return ret;
+				},
+			},
+			timestamps,
+        }
+    }
+}
