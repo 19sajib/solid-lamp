b56a5a8d0d670943f8af6d365437be1d90bf2142
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-18 22:21:06 +0600

    connect mongodb with typegoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configValidationSchema } from './config/config.schema';
 import config from './config/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    cache: true,
-    isGlobal: true,
-    load: [config],
-    validationSchema: configValidationSchema
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [config],
+      validationSchema: configValidationSchema 
+    }),
+    TypegooseModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get("MONGODB_URI"),
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
