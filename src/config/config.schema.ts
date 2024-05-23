d7cb99f4c4c2d832545e48d355e694f5689069e8
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-23 19:31:05 +0600

    jwt register

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -3,5 +3,5 @@ import * as Joi from 'joi';
 export const configValidationSchema = Joi.object({
     MONGODB_URI: Joi.string().required(),
     JWT_SECRET_KEY: Joi.string().required(),
-    JWT_EXPIRE_LIMIT: Joi.number().required()
+    JWT_EXPIRE_LIMIT: Joi.string().required()
 });
