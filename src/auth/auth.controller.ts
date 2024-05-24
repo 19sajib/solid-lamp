a3cdd78f75c025e1a0d51deb4f7e386d93abe597
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-24 23:09:15 +0600

    general login & register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDTO } from './dto/login.dto';
+import { RegisterDTO } from './dto/register.dto';
 
 @ApiTags("Auth Related APIs")
 @Controller('auth')
@@ -20,5 +22,15 @@ export class AuthController {
     @UseGuards(AuthGuard('google'))
     async googleRedirect(@Req() req) {
         return await this.authService.GoogleAuth(req.user)
+    }
+
+    @Post('login')
+    async login(@Body() body: LoginDTO): Promise<any> {
+        return await this.authService.login(body)
+    }
+
+    @Post('register')
+    async register(@Body() body: RegisterDTO): Promise <any> {
+        return await this.authService.Register(body)
     }
 }
