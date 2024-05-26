937cab15c5a5668cb9358ff5b0557999fdd9d742
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-26 18:37:45 +0600

    Guard

---
+++ src/utils/guard/auth.guard.ts
@@ -0,0 +1,44 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { AuthService } from "src/auth/auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private jwtService: JwtService,
+        private authService: AuthService,
+        private configService: ConfigService
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean>{
+        const request = context.switchToHttp().getRequest()
+        const token = this.extractTokenFromHeader(request)
+        if(!token) throw new UnauthorizedException()
+        
+        try {
+            const payload = await this.jwtService.verifyAsync(
+                token,
+                {
+                    secret: this.configService.get('JWT_SECRET_KEY')
+                }
+            )
+
+            const user = await this.authService.getUserById(payload.id)
+            if (!user) throw new UnauthorizedException()
+            
+            request['user'] = user.id
+            
+        } catch {
+            throw new UnauthorizedException()
+        }
+        
+        return true
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? []
+        return type === 'Bearer' ? token : undefined
+    }
+}
