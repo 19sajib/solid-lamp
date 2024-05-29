a47e2d301f064960d6d30c051fe3db93d7f49a1a
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-05-29 23:06:53 +0600

    review dto & review entity revised

---
+++ src/company/dto/review.dto.ts
@@ -0,0 +1,41 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { EmploymentStatus, EmploymentType } from "../entity/helper";
+import { IsNumber, IsOptional, IsString, Max, Min } from "class-validator";
+
+export class ReviewDTo {
+    @ApiProperty({ required: true, enum: EmploymentStatus, example: EmploymentStatus.FULL_TIME })
+    employmentStatus: EmploymentStatus
+
+    @ApiProperty({ required: true, enum: EmploymentType, example: EmploymentType.FORMER })
+    employmentType: EmploymentType
+
+    @ApiProperty({ required: true, type: String, example: "Software Engineer"})
+    @IsString()
+    position: string
+
+    @ApiProperty({ required: true, type: String, example: "Best For Software Engineer"})
+    @IsString()
+    reviewTitle: string
+
+    @ApiProperty({ required: true, type: Number, example: 5 })
+    @IsNumber()
+    @Min(1)
+    @Max(5)
+    rating: number
+
+    @ApiProperty({ required: true, type: String })
+    @IsString()
+    pros: string
+
+    @ApiProperty({ required: true, type: String })
+    @IsString()
+    cons: string
+
+    @ApiProperty({ type: String })
+    @IsString()
+    @IsOptional()
+    adviceForManagement: string
+
+    @ApiProperty({ type: Boolean })
+    recommended: boolean
+}
