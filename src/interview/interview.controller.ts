11fefd4becb241601b8a5ccff1c5e5a145d662f5
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-06-26 15:36:48 +0600

    interview controller

---
+++ src/interview/interview.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { InterviewService } from './interview.service';
+import { AuthGuard } from 'src/utils/guard/auth.guard';
+import { InterviewDTO } from './dto/interview.dto';
+import { AdminGuard } from 'src/utils/guard/admin.guard';
+
+@ApiTags('Interview Related APIs')
+@Controller('interview')
+@ApiBearerAuth()
+export class InterviewController {
+    constructor(
+        private readonly interviewService: InterviewService
+    ) {}
+
+    @UseGuards(AuthGuard)
+    @Get(':companyId')
+    async getInterviewList(@Param('companyId') companyId: string) {
+        return this.interviewService.getInterviewList(companyId)
+    }
+
+    @UseGuards(AuthGuard)
+    @Post(':companyId')
+    async addInterview(@Body() body: InterviewDTO, @Req() req, @Param('companyId') companyId: string) {
+        body.reviewer = req.user
+        body.companyId = companyId
+        return await this.interviewService.addInterview(body)
+    }
+
+    @UseGuards(AdminGuard)
+    @Patch(':interviewId')
+    async archiveInterview(@Param('interviewId') interviewId: string) {
+        return await this.interviewService.archiveInterview(interviewId)
+    }
+}
