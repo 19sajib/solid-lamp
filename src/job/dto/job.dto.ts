4a4f2083c28beb1e11661835ba71264a7188ae60
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-06-28 15:21:34 +0600

    job entity & dto

--- src/job/dto/job.dto.ts
+++ src/job/dto/job.dto.ts
@@ -1,9 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
-import { EmploymentStatus, WorkType } from "../entity/helper";
+import { IsArray, IsMongoId, IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { EmploymentStatus, WorkType } from "../helper";
 
 export class JobDTO {
-    @ApiProperty({ type: String })
     @IsMongoId()
     @IsOptional()
     companyId: string
@@ -14,17 +13,33 @@ export class JobDTO {
 
     @ApiProperty({ type: String, example: "Full Stack Developer", required: true })
     @IsString()
+    @IsNotEmpty()
     position: string
 
     @ApiProperty({ type: String, required: true })
     @IsString()
+    @IsNotEmpty()
     description: string
 
     @ApiProperty({ enum: EmploymentStatus, example: EmploymentStatus.FULL_TIME, required: true})
+    @IsString()
+    @IsNotEmpty()
     employmentStatus: EmploymentStatus
 
     @ApiProperty({ enum: WorkType, example: WorkType.HYBRID, required: true })
+    @IsString()
+    @IsNotEmpty()
     workType: WorkType
+
+    @ApiProperty({ type: String, example: "Sun-Thu"})
+    @IsNotEmpty()
+    @IsString()
+    workDays: string
+
+    @ApiProperty({ type: String, example: "9-5"})
+    @IsNotEmpty()
+    @IsString()
+    workingHours: string
 
     @ApiProperty({ type: String, example: "London"})
     @IsString()
@@ -45,5 +60,15 @@ export class JobDTO {
     @IsString()
     @IsNotEmpty()
     salaryRange: string
+
+    @ApiProperty({ required: true, type: String, example: "Fresher"})
+    @IsString()
+    @IsNotEmpty()
+    experiencedRequired: string
+
+    @ApiProperty({ required: true, type: () => [String], example: ["Node", "MongoDB"]})
+    @IsArray()
+    @IsNotEmpty()
+    techStack: string[]
 
 }
