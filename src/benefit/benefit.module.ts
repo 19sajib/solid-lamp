40caf65930ca40e175514c1828ff61c7bd58c379
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-07-07 22:44:44 +0600

    benefit module

--- src/benefit/benefit.module.ts
+++ src/benefit/benefit.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BenefitController } from './benefit.controller';
 import { BenefitService } from './benefit.service';
 import { TypegooseModule } from 'nestjs-typegoose';
-import { Benefit } from './entity/benefit.entity';
+import { Benefit, BenefitReview } from './entity/benefit.entity';
 import { User } from 'src/auth/entity/user.entity';
 import { Company } from 'src/company/entity/company.entity';
 import { JwtService } from '@nestjs/jwt';
@@ -10,7 +10,7 @@ import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports:[
-    TypegooseModule.forFeature([Benefit, User, Company])
+    TypegooseModule.forFeature([Benefit, BenefitReview, User, Company])
   ],
   controllers: [BenefitController],
   providers: [BenefitService, JwtService, AuthService]
