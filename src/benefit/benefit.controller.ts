c8a30c73d98361def0d1072e2f173dc7887ba7fc
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-07-07 22:44:10 +0600

    benefit controller

--- src/benefit/benefit.controller.ts
+++ src/benefit/benefit.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { BenefitService } from './benefit.service';
+import { AuthGuard } from 'src/utils/guard/auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { BenefitReviewDTO } from './dto/benefit.dto';
+import { AdminGuard } from 'src/utils/guard/admin.guard';
 
-@Controller('Benefit Related APIs')
-export class BenefitController {}
+@ApiTags('Benefit Related APIs')
+@ApiBearerAuth()
+@Controller('benefit')
+export class BenefitController {
+  constructor(private readonly benefitService: BenefitService) {}
+
+  @UseGuards(AuthGuard)
+  @Get(':companyId')
+  async getBenefitByCompanyId(@Param('companyId') companyId: string) {
+    return await this.benefitService.getBenefitByCompanyId(companyId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('review/:companyId')
+  async getBenefitReviewByCompanyId(@Param('companyId') companyId: string) {
+    return await this.benefitService.getBenefitReviewByCompanyId(companyId);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post(':companyId')
+  async addBenefit(
+    @Body() body: BenefitReviewDTO,
+    @Req() req,
+    @Param('companyId') companyId: string,
+  ) {
+    body.reviewer = req.user;
+    body.companyId = companyId;
+    return await this.benefitService.addBenefit(body);
+  }
+
+  @UseGuards(AdminGuard)
+  @Patch('review/:benefitReviewId')
+  async archiveBenefitReview(@Param('benefitReviewId') benefitReviewId: string) {
+    return await this.benefitService.archiveBenefitReview(benefitReviewId)
+  }
+}
