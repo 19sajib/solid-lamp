d68e363a0111c9db40e994f3a567a64f16a1cb3e
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-08-24 15:44:08 +0600

    life style module

--- src/lifestyle/lifestyle.module.ts
+++ src/lifestyle/lifestyle.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { LifestyleController } from './lifestyle.controller';
-import { LifestyleService } from './lifestyle.service';
+import { LifeStyleController } from './lifestyle.controller';
+import { LifeStyleService } from './lifestyle.service';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { LifeStyle, LifeStyleUpload } from './entity/LifeStyle.entity';
+import { JwtService } from '@nestjs/jwt';
+import { AuthService } from 'src/auth/auth.service';
+import { User } from 'src/auth/entity/user.entity';
 
 @Module({
-  controllers: [LifestyleController],
-  providers: [LifestyleService, CloudinaryService]
+  imports: [
+    TypegooseModule.forFeature([LifeStyle, LifeStyleUpload, User])
+  ],
+  controllers: [LifeStyleController],
+  providers: [LifeStyleService, CloudinaryService, JwtService, AuthService]
 })
 export class LifestyleModule {}
